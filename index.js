90f8da13a5e67431a1d11a0b8e71f3f8c53bacf6
Alexandra Weaver <aweaver0124@gmail.com>
2021-11-11 05:02:48 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+function increment(n) {
+    return n += 1;
+}
+function decrement(n) {
+    return n -= 1;
+}
+function makeInt(n) {
+    return parseInt(n,10);
+}
+makeInt(2);
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+parseFloat('2.222');
+
